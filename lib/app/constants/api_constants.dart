ad73f22e5555e2e61dd52f19511752cf15e8c641
gajeraajay2001 <gajeraajay2001@gmail.com>
2022-10-10 14:18:30 +0530

    Api Implementation started.

--- lib/app/constants/api_constants.dart
+++ lib/app/constants/api_constants.dart
@@ -1,1 +1,7 @@
 const imagePath = "assets/images/";
+const baseUrl = "https://alldogs.in/api/";
+const imageBaseUrl = "https://alldogs.in/public/uploads/";
+
+class ApiConstant {
+  static const homeDataApi = "home";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:all_dogs/app/constants/color_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,9 +11,20 @@ import 'package:get_storage/get_storage.dart';
 import 'app/constants/app_module.dart';
 import 'app/routes/app_pages.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 final getIt = GetIt.instance;
 GetStorage box = GetStorage();
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = new MyHttpOverrides();
   await GetStorage.init();
   setUp();
   runApp(
